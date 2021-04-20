2a292b4f67394794ed24b02916105174ac7cd9eb
root <root@ip-172-31-20-155.ca-central-1.compute.internal>
2021-04-20 07:31:16 +0000

    Added parameter IAM_user

--- list_ec2.sh
+++ list_ec2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #this script will list out all instances in Canada and tokyo (InstancesType, PublicIpV4, Ec2-name, Stopped/running)
+#Included Instance name and state to differentiate between other EC2 instances I already use in my AWS account
+IAM_user=$1
 rm -rf /home/ubuntu/my_ec2.txt
-aws ec2 describe-instances --region ap-northeast-1 --profile dalailama | grep -E 'InstanceType|PublicIpAddress|Value|"Name":' >> /home/ubuntu/my_ec2.txt
-aws ec2 describe-instances --region ca-central-1 --profile dalailama | grep -E 'InstanceType|PublicIpAddress|Value|"Name":' >> /home/ubuntu/my_ec2.txt
+aws ec2 describe-instances --region ap-northeast-1 --profile $IAM_user | grep -E 'InstanceType|PublicIpAddress|Value|"Name":' >> /home/ubuntu/my_ec2.txt
+aws ec2 describe-instances --region ca-central-1 --profile $IAM_user | grep -E 'InstanceType|PublicIpAddress|Value|"Name":' >> /home/ubuntu/my_ec2.txt
 cat /home/ubuntu/my_ec2.txt
